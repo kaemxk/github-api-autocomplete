b8e7b405224bb35843d9c7c466754710e3f3ce04
kaemxk <kaemxk@gmail.com>
2023-01-13 19:56:48 +0300

    ну щас точно готово

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ const searchWrapper = document.querySelector('.search__wrapper')
 const repoWrapper = document.querySelector('.repo-list__wrapper')
 
 function debounce(cb, time) {
-    let timer
-    return function (...args) {
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-            cb.apply(this, args)
-        }, time)
-    }
+  let timer
+  return function (...args) {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      cb.apply(this, args)
+    }, time)
+  }
 }
 
 function getRepos(text) {
-    return Promise.resolve()
-        .then(() => {
-            return fetch(`https://api.github.com/search/repositories?q=${text}&per_page=5`)
-                .then(response => response.json())
-        })
+  return Promise.resolve()
+    .then(() => {
+      return fetch(`https://api.github.com/search/repositories?q=${text}&per_page=5`)
+        .then(response => response.json())
+    })
 }
 
 function addRepo(data) {
-    const repoItem = document.createElement('div')
-    repoItem.classList.add('repo-list__item', 'item')
+  const repoItem = document.createElement('div')
+  repoItem.classList.add('repo-list__item', 'item')
 
-    repoItem.innerHTML = `
+  repoItem.innerHTML = `
         <div class="item__col">
             <div class="item__name">Name: ${data.name}</div>
             <div class="item__owner">Owner: ${data.owner.login}</div>
@@ -34,53 +34,54 @@ function addRepo(data) {
             <button class="item__delete"><img src="cross.svg" alt="delete"></button>
         </div>
     `
-    repoWrapper.append(repoItem)
+  repoWrapper.append(repoItem)
 }
 
 function createDropdown(data) {
-    try {
-        const arr = data.items
-        const dropdown = document.createElement('div')
-        dropdown.classList.add('dropdown')
-        arr.forEach(item => {
-            const dropdownItem = document.createElement('div')
-            dropdownItem.addEventListener('click', () => {
-                addRepo(item)
-            })
-            dropdownItem.classList.add('dropdown__item')
-            const dropdownTitle = document.createElement('div')
-            dropdownTitle.classList.add('dropdown__title')
-            dropdownTitle.textContent = item.name
-            dropdownItem.append(dropdownTitle)
-            dropdown.append(dropdownItem)
-        })
-        searchWrapper.append(dropdown)
-    } catch (e) {
-    }
+  try {
+    const arr = data.items
+    const dropdown = document.createElement('div')
+    dropdown.classList.add('dropdown')
+    arr.forEach(item => {
+      const dropdownItem = document.createElement('div')
+      dropdownItem.addEventListener('click', () => {
+        addRepo(item)
+      })
+      dropdownItem.classList.add('dropdown__item')
+      const dropdownTitle = document.createElement('div')
+      dropdownTitle.classList.add('dropdown__title')
+      dropdownTitle.textContent = item.name
+      dropdownItem.append(dropdownTitle)
+      dropdown.append(dropdownItem)
+    })
+    searchWrapper.append(dropdown)
+  } catch (e) {
+  }
 }
 
-input.addEventListener('input', debounce(() => {
-    if (input.value !== '') {
-        getRepos(input.value)
-            .then(data => {
-                createDropdown(data)
-            })
-    } else {
-        const dropdown = document.querySelector('.dropdown')
-        dropdown.remove()
-    }
+function removeDropdown() {
+  const dropdown = document.querySelector('.dropdown')
+  dropdown.remove()
+  input.value = ''
+}
+
+input.addEventListener('input', debounce(async () => {
+  if (input.value !== '') {
+    const data = await getRepos(input.value)
+    createDropdown(data)
+  } else {
+    removeDropdown()
+  }
 }, 500))
 
 searchWrapper.addEventListener('click', (e) => {
-    if (e.target.classList.contains('dropdown__title')) {
-        input.value = ''
-        const dropdown = document.querySelector('.dropdown')
-        dropdown.remove()
-    }
+  if (e.target.classList.contains('dropdown__title')) {
+    removeDropdown()
+  }
 })
 
 repoWrapper.addEventListener('click', (e) => {
-    if (e.target.nodeName === 'IMG' || e.target.nodeName === 'BUTTON') {
-        e.target.closest('.item').remove()
-    }
+  if (e.target.nodeName === 'IMG' || e.target.nodeName === 'BUTTON') {
+    e.target.closest('.item').remove()
+  }
 })
